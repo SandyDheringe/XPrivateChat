89ceb3048a5b517237618725dbf7476b4565c536
PEBSTONE\sandeep.dheringe <sandeep.dheringe@pebstone.com>
2019-02-22 17:39:22 +0530

    Initial commit

---
+++ app/src/main/java/com/example/exoplayerdemo/ChatListActivity.java
@@ -0,0 +1,28 @@
+package com.example.exoplayerdemo;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+
+public class ChatListActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.etUsername)
+    EditText etUsername;
+
+    @BindView(R.id.etPassword)
+    EditText etPassword;
+
+    @BindView(R.id.btnLogin)
+    Button btnLogin;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example/exoplayerdemo/RegisterActivity1.java
@@ -0,0 +1,24 @@
+package com.example.exoplayerdemo;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+
+public class RegisterActivity1 extends AppCompatActivity {
+
+
+    @BindView(R.id.etUsername)
+    EditText etUsername;
+
+    @BindView(R.id.etPassword)
+    EditText etPassword;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_1);
+    }
+}
