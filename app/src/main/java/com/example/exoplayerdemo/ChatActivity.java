876f3b5b7537761e778662c63a1eef61039d00cc
Sandeep Dheringe <sandeep.dheringe90@gmail.com>
2019-03-03 01:07:58 +0530

    -audio and video chat screen added

--- app/src/main/java/com/example/exoplayerdemo/ChatActivity.java
+++ app/src/main/java/com/example/exoplayerdemo/ChatActivity.java
@@ -1,9 +1,10 @@
 package com.example.exoplayerdemo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.exoplayerdemo.adapter.ChatAdapter;
-import com.example.exoplayerdemo.adapter.ChatListAdapter;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -11,13 +12,17 @@ import androidx.recyclerview.widget.RecyclerView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class ChatActivity extends AppCompatActivity {
+public class ChatActivity extends AppCompatActivity
+{
 
     @BindView(R.id.rvChatList)
     RecyclerView rvChatList;
+    @BindView(R.id.btnSend)
+    Button btnSend;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
@@ -26,6 +31,9 @@ public class ChatActivity extends AppCompatActivity {
         rvChatList.setLayoutManager(new LinearLayoutManager(this));
         rvChatList.setAdapter(new ChatAdapter());
 
+        btnSend.setOnClickListener(v -> {
+            startActivity(new Intent(ChatActivity.this, AudioCallActivity.class));
+        });
 
     }
 }
---
+++ app/src/main/java/com/example/exoplayerdemo/VideoCallActivity.java
@@ -0,0 +1,18 @@
+package com.example.exoplayerdemo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class VideoCallActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_call);
+
+    }
+
+}
